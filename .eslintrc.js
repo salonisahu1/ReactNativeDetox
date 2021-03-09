a2588cc74047b14fff92122d72efe83a954ce73e
Saloni Sahu <saloni@geekyants.com>
2021-03-09 18:46:23 +0530

    detox initial setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,5 +3,16 @@ module.exports = {
   extends: '@react-native-community',
   rules: {
     no-inline-styles: false
-  }
+  },
+  plugins: ['detox'],
+  overrides: [
+    {
+      files: ['*.e2e.js'],
+      env: {
+        'detox/detox': true,
+        jest: true,
+        'jest/globals': true,
+      },
+    },
+  ],
 };
