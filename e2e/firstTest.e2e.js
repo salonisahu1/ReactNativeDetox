fc66c973e72ac051a53d4077cc7226c76a56b998
Saloni Sahu <saloni@geekyants.com>
2021-03-11 19:47:36 +0530

    Android setup

--- e2e/firstTest.e2e.js
+++ e2e/firstTest.e2e.js
@@ -12,11 +12,9 @@ describe('Example', () => {
     await element(by.id('Email')).typeText('saloni@geekyants.com');
     await element(by.id('Password')).typeText('goldtree9');
     await element(by.id('LoginButton')).tap();
-    await expect(element(by.id('WelcomeText'))).toBeVisible();
-  });
-
-  it('should logout successfully', async () => {
-    await expect(element(by.id('WelcomeText'))).toBeVisible();
+    await waitFor(element(by.id('WelcomeText')))
+      .toBeVisible()
+      .withTimeout(2000);
     await element(by.id('LogoutButton')).tap();
   });
 });
